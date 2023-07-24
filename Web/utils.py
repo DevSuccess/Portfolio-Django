d2de24a13a8dabdfb237a568d11e746b94cb3a80
ITDevSuccess <raharisontsidiany@gmail.com>
2023-07-24 21:32:23 +0300

    Ajout de fichier utils

---
+++ Web/utils.py
@@ -0,0 +1,331 @@
+from uuid import uuid4
+import os
+from django.db import models
+from django.core.validators import FileExtensionValidator
+from django.utils.safestring import mark_safe
+
+DAYS = (
+    ('Monday', 'Monday'),
+    ('Tuesday', 'Tuesday'),
+    ('Wednesday', 'Wednesday'),
+    ('Thursday', 'Thursday'),
+    ('Friday', 'Friday'),
+    ('Saturday', 'Saturday'),
+    ('Sunday', 'Sunday')
+)
+
+SOCIALS = (
+    ('fab fa-linkedin-in', 'LinkedIn'),
+    ('fab fa-twitter', 'Twitter'),
+    ('fab fa-facebook-f', 'Facebook'),
+    ('fab fa-instagram', 'Instagram'),
+)
+
+STATES = sorted([
+    ('Andaman & Nicobar Islands', 'Andaman & Nicobar Islands'),
+    ('Andhra Pradesh', 'Andhra Pradesh'),
+    ('Arunachal Pradesh', 'Arunachal Pradesh'),
+    ('Assam', 'Assam'),
+    ('Bihar', 'Bihar'),
+    ('Chandigarh', 'Chandigarh'),
+    ('Chhattisgarh', 'Chhattisgarh'),
+    ('Dadra & Nagar Haveli', 'Dadra & Nagar Haveli'),
+    ('Daman & Diu', 'Daman & Diu'),
+    ('Delhi', 'Delhi'),
+    ('Goa', 'Goa'),
+    ('Gujarat', 'Gujarat'),
+    ('Haryana', 'Haryana'),
+    ('Himachal Pradesh', 'Himachal Pradesh'),
+    ('Jammu & Kashmir', 'Jammu & Kashmir'),
+    ('Jharkhand', 'Jharkhand'),
+    ('Karnataka', 'Karnataka'),
+    ('Kerala', 'Kerala'),
+    ('Lakshadweep', 'Lakshadweep'),
+    ('Madhya Pradesh', 'Madhya Pradesh'),
+    ('Maharashtra', 'Maharashtra'),
+    ('Manipur', 'Manipur'),
+    ('Meghalaya', 'Meghalaya'),
+    ('Mizoram', 'Mizoram'),
+    ('Nagaland', 'Nagaland'),
+    ('Odisha', 'Odisha'),
+    ('Puducherry', 'Puducherry'),
+    ('Punjab', 'Punjab'),
+    ('Rajasthan', 'Rajasthan'),
+    ('Sikkim', 'Sikkim'),
+    ('Tamil Nadu', 'Tamil Nadu'),
+    ('Telangana', 'Telangana'),
+    ('Tripura', 'Tripura'),
+    ('Uttar Pradesh', 'Uttar Pradesh'),
+    ('Uttarakhand', 'Uttarakhand'),
+    ('Afghanistan', 'Afghanistan'),
+    ('Albania', 'Albania'),
+    ('Algeria', 'Algeria'),
+    ('Andorra', 'Andorra'),
+    ('Angola', 'Angola'),
+    ('Antigua and Barbuda', 'Antigua and Barbuda'),
+    ('Argentina', 'Argentina'),
+    ('Armenia', 'Armenia'),
+    ('Australia', 'Australia'),
+    ('Austria', 'Austria'),
+    ('Azerbaijan', 'Azerbaijan'),
+    ('Bahamas', 'Bahamas'),
+    ('Bahrain', 'Bahrain'),
+    ('Bangladesh', 'Bangladesh'),
+    ('Barbados', 'Barbados'),
+    ('Belarus', 'Belarus'),
+    ('Belgium', 'Belgium'),
+    ('Belize', 'Belize'),
+    ('Benin', 'Benin'),
+    ('Bhutan', 'Bhutan'),
+    ('Bolivia', 'Bolivia'),
+    ('Bosnia and Herzegovina', 'Bosnia and Herzegovina'),
+    ('Botswana', 'Botswana'),
+    ('Brazil', 'Brazil'),
+    ('Brunei', 'Brunei'),
+    ('Bulgaria', 'Bulgaria'),
+    ('Burkina Faso', 'Burkina Faso'),
+    ('Burundi', 'Burundi'),
+    ('Cabo Verde', 'Cabo Verde'),
+    ('Cambodia', 'Cambodia'),
+    ('Cameroon', 'Cameroon'),
+    ('Canada', 'Canada'),
+    ('Central African Republic', 'Central African Republic'),
+    ('Chad', 'Chad'),
+    ('Chile', 'Chile'),
+    ('China', 'China'),
+    ('Colombia', 'Colombia'),
+    ('Comoros', 'Comoros'),
+    ('Congo, Democratic Republic of the', 'Congo, Democratic Republic of the'),
+    ('Congo, Republic of the', 'Congo, Republic of the'),
+    ('Costa Rica', 'Costa Rica'),
+    ('Croatia', 'Croatia'),
+    ('Cuba', 'Cuba'),
+    ('Cyprus', 'Cyprus'),
+    ('Czech Republic', 'Czech Republic'),
+    ('Denmark', 'Denmark'),
+    ('Djibouti', 'Djibouti'),
+    ('Dominica', 'Dominica'),
+    ('Dominican Republic', 'Dominican Republic'),
+    ('East Timor', 'East Timor'),
+    ('Ecuador', 'Ecuador'),
+    ('Egypt', 'Egypt'),
+    ('El Salvador', 'El Salvador'),
+    ('Equatorial Guinea', 'Equatorial Guinea'),
+    ('Eritrea', 'Eritrea'),
+    ('Estonia', 'Estonia'),
+    ('Eswatini', 'Eswatini'),
+    ('Ethiopia', 'Ethiopia'),
+    ('Fiji', 'Fiji'),
+    ('Finland', 'Finland'),
+    ('France', 'France'),
+    ('Gabon', 'Gabon'),
+    ('Gambia', 'Gambia'),
+    ('Georgia', 'Georgia'),
+    ('Germany', 'Germany'),
+    ('Ghana', 'Ghana'),
+    ('Greece', 'Greece'),
+    ('Grenada', 'Grenada'),
+    ('Guatemala', 'Guatemala'),
+    ('Guinea', 'Guinea'),
+    ('Guinea-Bissau', 'Guinea-Bissau'),
+    ('Guyana', 'Guyana'),
+    ('Haiti', 'Haiti'),
+    ('Honduras', 'Honduras'),
+    ('Hungary', 'Hungary'),
+    ('Iceland', 'Iceland'),
+    ('India', 'India'),
+    ('Indonesia', 'Indonesia'),
+    ('Iran', 'Iran'),
+    ('Iraq', 'Iraq'),
+    ('Ireland', 'Ireland'),
+    ('Israel', 'Israel'),
+    ('Italy', 'Italy'),
+    ('Jamaica', 'Jamaica'),
+    ('Japan', 'Japan'),
+    ('Jordan', 'Jordan'),
+    ('Kazakhstan', 'Kazakhstan'),
+    ('Kenya', 'Kenya'),
+    ('Kiribati', 'Kiribati'),
+    ('Korea, North', 'Korea, North'),
+    ('Korea, South', 'Korea, South'),
+    ('Kosovo', 'Kosovo'),
+    ('Kuwait', 'Kuwait'),
+    ('Kyrgyzstan', 'Kyrgyzstan'),
+    ('Laos', 'Laos'),
+    ('Latvia', 'Latvia'),
+    ('Lebanon', 'Lebanon'),
+    ('Lesotho', 'Lesotho'),
+    ('Liberia', 'Liberia'),
+    ('Libya', 'Libya'),
+    ('Liechtenstein', 'Liechtenstein'),
+    ('Lithuania', 'Lithuania'),
+    ('Luxembourg', 'Luxembourg'),
+    ('Madagascar', 'Madagascar'),
+    ('Malawi', 'Malawi'),
+    ('Malaysia', 'Malaysia'),
+    ('Maldives', 'Maldives'),
+    ('Mali', 'Mali'),
+    ('Malta', 'Malta'),
+    ('Marshall Islands', 'Marshall Islands'),
+    ('Mauritania', 'Mauritania'),
+    ('Mauritius', 'Mauritius'),
+    ('Mexico', 'Mexico'),
+    ('Micronesia', 'Micronesia'),
+    ('Moldova', 'Moldova'),
+    ('Monaco', 'Monaco'),
+    ('Mongolia', 'Mongolia'),
+    ('Montenegro', 'Montenegro'),
+    ('Morocco', 'Morocco'),
+    ('Mozambique', 'Mozambique'),
+    ('Myanmar', 'Myanmar'),
+    ('Namibia', 'Namibia'),
+    ('Nauru', 'Nauru'),
+    ('Nepal', 'Nepal'),
+    ('Netherlands', 'Netherlands'),
+    ('New Zealand', 'New Zealand'),
+    ('Nicaragua', 'Nicaragua'),
+    ('Niger', 'Niger'),
+    ('Nigeria', 'Nigeria'),
+    ('North Macedonia', 'North Macedonia'),
+    ('Norway', 'Norway'),
+    ('Oman', 'Oman'),
+    ('Pakistan', 'Pakistan'),
+    ('Palau', 'Palau'),
+    ('Palestine', 'Palestine'),
+    ('Panama', 'Panama'),
+    ('Papua New Guinea', 'Papua New Guinea'),
+    ('Paraguay', 'Paraguay'),
+    ('Peru', 'Peru'),
+    ('Philippines', 'Philippines'),
+    ('Poland', 'Poland'),
+    ('Portugal', 'Portugal'),
+    ('Qatar', 'Qatar'),
+    ('Romania', 'Romania'),
+    ('Russia', 'Russia'),
+    ('Rwanda', 'Rwanda'),
+    ('Saint Kitts and Nevis', 'Saint Kitts and Nevis'),
+    ('Saint Lucia', 'Saint Lucia'),
+    ('Saint Vincent and the Grenadines', 'Saint Vincent and the Grenadines'),
+    ('Samoa', 'Samoa'),
+    ('San Marino', 'San Marino'),
+    ('Sao Tome and Principe', 'Sao Tome and Principe'),
+    ('Saudi Arabia', 'Saudi Arabia'),
+    ('Senegal', 'Senegal'),
+    ('Serbia', 'Serbia'),
+    ('Seychelles', 'Seychelles'),
+    ('Sierra Leone', 'Sierra Leone'),
+    ('Singapore', 'Singapore'),
+    ('Slovakia', 'Slovakia'),
+    ('Slovenia', 'Slovenia'),
+    ('Solomon Islands', 'Solomon Islands'),
+    ('Somalia', 'Somalia'),
+    ('South Africa', 'South Africa'),
+    ('South Sudan', 'South Sudan'),
+    ('Spain', 'Spain'),
+    ('Sri Lanka', 'Sri Lanka'),
+    ('Sudan', 'Sudan'),
+    ('Suriname', 'Suriname'),
+    ('Sweden', 'Sweden'),
+    ('Switzerland', 'Switzerland'),
+    ('Syria', 'Syria'),
+    ('Taiwan', 'Taiwan'),
+    ('Tajikistan', 'Tajikistan'),
+    ('Tanzania', 'Tanzania'),
+    ('Thailand', 'Thailand'),
+    ('Togo', 'Togo'),
+    ('Tonga', 'Tonga'),
+    ('Trinidad and Tobago', 'Trinidad and Tobago'),
+    ('Tunisia', 'Tunisia'),
+    ('Turkey', 'Turkey'),
+    ('Turkmenistan', 'Turkmenistan'),
+    ('Tuvalu', 'Tuvalu'),
+    ('Uganda', 'Uganda'),
+    ('Ukraine', 'Ukraine'),
+    ('United Arab Emirates', 'United Arab Emirates'),
+    ('United Kingdom', 'United Kingdom'),
+    ('United States', 'United States'),
+    ('Uruguay', 'Uruguay'),
+    ('Uzbekistan', 'Uzbekistan'),
+    ('Vanuatu', 'Vanuatu'),
+    ('Vatican City', 'Vatican City'),
+    ('Venezuela', 'Venezuela'),
+    ('Vietnam', 'Vietnam'),
+    ('Yemen', 'Yemen'),
+    ('Zambia', 'Zambia'),
+    ('Zimbabwe', 'Zimbabwe')
+], key=lambda x: x[1])
+
+
+def filetype(filename):
+    # Obtenir l'extension du fichier
+    _, ext = os.path.splitext(filename)
+    ext = ext.lower()
+
+    # Vérification si c'est une image
+    image_extensions = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.webp', '.svg']
+    if ext in image_extensions:
+        return 'image'
+
+    # Vérification si c'est une vidéo
+    video_extensions = ['.mp4', '.avi', '.mov']
+    if ext in video_extensions:
+        return 'video'
+
+    # Aucun type correspondant trouvé
+    return 'unknown'
+
+
+def upload_path(instance, filename):
+    # Obtenir l'extension du fichier
+    _, ext = os.path.splitext(filename)
+    ext = ext.lower()
+
+    # Générer un nom de fichier unique
+    unique_filename = f"{uuid4().hex}{ext}"
+
+    return os.path.join(str(filetype(filename)), unique_filename)
+
+
+class BaseModel(models.Model):
+    active = models.BooleanField(default=True, )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class ImageModel(models.Model):
+    image = models.FileField(
+        upload_to=upload_path,
+        validators=[
+            FileExtensionValidator(allowed_extensions=['png', 'jpg', 'gif', 'jpeg', 'webp', 'svg', 'bmp'])
+        ],
+        default='', null=True, blank=True
+    )
+
+    def admin_photo(self):
+        if self.image:
+            return mark_safe(f"<a href='{self.image.url}'><img src='{self.image.url}' width='100' /></a>")
+        else:
+            return ""
+
+    class Meta:
+        abstract = True
+
+
+class VideoModel(models.Model):
+    video = models.FileField(
+        upload_to=upload_path,
+        validators=[
+            FileExtensionValidator(allowed_extensions=['mp4', 'avi', 'mov'])
+        ], default='')
+
+    def admin_video(self):
+        if self.video:
+            return mark_safe(f"<video width='150' controls ><source  type='video/mp4' src='{self.video.url}'></video>")
+        else:
+            return ""
+
+    class Meta:
+        abstract = True
